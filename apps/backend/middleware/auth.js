45beffb68bbe64a7f2bd52bb5ee98ea77e8f502a
김원태 <rladnjsxo123@naver.com>
2025-04-07 18:49:13 +0900

    chore: 로그를 제거합니다. (#34)

--- apps/backend/middleware/auth.js
+++ apps/backend/middleware/auth.js
@@ -2,14 +2,12 @@ import jwt from "jsonwebtoken";
 
 export const authenticateJWT = (req, res, next) => {
   const token = req.cookies?.token;
-  console.log("token", token);
   if (!token) {
     return res.status(401).json({ error: "Unauthorized: No token provided" });
   }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    console.log(decoded);
     req.user = decoded; // 유저 정보 저장
     next();
   } catch (err) {
